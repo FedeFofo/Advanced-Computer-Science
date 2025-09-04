47642a2165140da4df7dd4f70c46e10755e1c7fc
FedeFofo <98142069+FedeFofo@users.noreply.github.com>
2025-09-03 20:19:25 -0700

    Added U0D3
    Notes in Lesson.java and created a v2 of Zombies.java based on what we learned

---
+++ Unit Lessons/U0D3/Zombie.java
@@ -0,0 +1,49 @@
+public class Zombie {
+    // instance variables (attributes)
+    // green
+    String skinColor = "Green";
+
+    // rot and dead
+    boolean isDead = true;
+
+    // humans eaten count
+    int humansEaten = 0;
+
+    // main
+    public static void main(String[] args) {
+        Zombie phillip = new Zombie();
+
+        System.out.println(phillip.growl());
+        System.out.println("Phillip has eaten " + phillip.humansEaten() + " humans.");
+
+        Zombie brian = new Zombie();
+
+        System.out.println(brian.growl());
+        System.out.println("Brian has eaten " + brian.humansEaten() + " humans.");
+        brian.eatHuman();
+        System.out.println("Brian has eaten " + brian.humansEaten() + " human.");
+        brian.feast(14);
+        System.out.println("Brian has eaten " + brian.humansEaten() + " humans.");
+    }
+
+    // methods (behaviors)
+    // walk with arms out
+    // eat brains
+    // growl
+    public String growl() {
+        return "grrrsssaaaajlhhhh...";
+    }
+
+    // Challenge: make a new method that returns the number of humans eaten
+    public int humansEaten() {
+        return humansEaten;
+    }
+
+    public void eatHuman() {
+        humansEaten++;
+    }
+
+    public void feast(int humanCount) {
+        humansEaten += humanCount;
+    }
+}
