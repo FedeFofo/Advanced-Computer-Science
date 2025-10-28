cb304dbc7cbfee735e8bad3c23b55b3289f3eaef
FedeFofo <98142069+FedeFofo@users.noreply.github.com>
2025-10-28 15:02:32 -0700

    Started PurrfectCare Lab
    Added Cat.java, PurrfectUtils.java, & CatTester.java

---
+++ Assignments/Unit 1 - PurrfectCare Lab/Cat.java
@@ -0,0 +1,95 @@
+public class Cat {
+    // instance variables
+    private String name;
+    private String ownerName;
+    private int moodLevel;
+    private String catId;
+    private char catChar;
+    private boolean isHungry;
+
+    // constructors
+    Cat(String name, String ownerName, int moodLevel, String catId) {
+        name = this.name;
+        ownerName = this.ownerName;
+        moodLevel = PurrfectUtils.validateMoodLevel(this.moodLevel);
+        catId = PurrfectUtils.validateCatId(this.catId);
+        catChar = PurrfectUtils.generateCatChar(catId);
+        isHungry = true;
+    }
+
+    Cat() {
+        name = "Garfield";
+        ownerName = "Jon Arbuckle";
+        moodLevel = 0;
+        catId = "2009";
+        catChar = PurrfectUtils.generateCatChar(catId);
+        isHungry = true;
+    }
+
+    // getters
+    public String getName() {
+        return name;
+    }
+
+    public String getOwnerName() {
+        return ownerName;
+    }
+
+    public int getMoodLevel() {
+        return moodLevel;
+    }
+
+    public String getCatId() {
+        return catId;
+    }
+
+    public char getCatChar() {
+        return catChar;
+    }
+
+    public boolean isHungry() {
+        return isHungry;
+    }
+
+    // setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setOwnerName(String ownerName) {
+        this.ownerName = ownerName;
+    }
+
+    public void setMoodLevel(int moodLevel) {
+        this.moodLevel = PurrfectUtils.validateMoodLevel(moodLevel);
+    }
+
+    public void setCatId(String catId) {
+        this.catId = PurrfectUtils.validateCatId(catId);
+    }
+
+    public void setCatChar(char catChar) {
+        this.catChar = catChar;
+    }
+
+    public void setHungry(boolean isHungry) {
+        this.isHungry = isHungry;
+    }
+
+    // methods
+    public String generateCatTag() {
+        return catId + catChar;
+    }
+
+    public String toString() {
+        String firstLine = "== ABOUT " + name + " ==";
+        String secondLine = name + " is a cat.";
+        String thirdLine = "Their tag is " + generateCatTag() + ".";
+        String fourthLine = PurrfectUtils.determineCatMood(this);
+        return firstLine + secondLine + thirdLine + fourthLine;
+    }
+
+    public boolean equals(Cat other) {
+        return name.equals(other.name) && ownerName.equals(other.ownerName) && moodLevel == other.moodLevel && generateCatTag().equals(other.generateCatTag()) && isHungry == other.isHungry;
+    }
+}
